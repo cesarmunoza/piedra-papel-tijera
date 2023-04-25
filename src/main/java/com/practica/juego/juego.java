140bcab000cc111e067f50c3950e421de7d119b6
cesar <cesare.munoz@gmail.com>
2023-04-25 13:47:45 -0500

    Se implementó el switch para la eleccion del ganador

--- src/main/java/com/practica/juego/juego.java
+++ src/main/java/com/practica/juego/juego.java
@@ -2,66 +2,52 @@ package com.practica.juego;
 
 import java.util.Scanner;
 
-public class juego {
-	private String piedra;
-	private String papel;
-	private String tijeras;
+public class juego {	
 	private static String eleccion1;
 	private static String eleccion2;
-	private static boolean gameover = false;
-		
+
 	public static void main(String[] args) {
 		jugar();
 	}
-	
+
 	public static void jugar() {
 		Scanner entrada = new Scanner(System.in);
-		
-		
-			System.out.println("Empieza el juego!!!");
-			System.out.println("****************************************************");
-			System.out.printf("%s%n","Jugador uno elija piedra papel o tijera: ");
-			eleccion1 = entrada.next();
-					
-			System.out.printf("%s%n","Jugador dos elija piedra papel o tijera: ");
-			eleccion2 = entrada.next();
-			
-			if (eleccion1.equals("piedra") && eleccion2.equals("piedra")) {
-				System.out.println("Los dos eligieron piedra, es un empate");
-				gameover = true;
-			}else if (eleccion1.equals("piedra") && eleccion2.equals("papel")) {
-				System.out.println("El jugador 2 gana. El papel envuelve a la piedra");
-				gameover = true;
-			}else if (eleccion1.equals("piedra") && eleccion2.equals("tijera")) {
-				System.out.println("El jugador 1 gana. La piedra quiebra la tijera");
-				gameover = true;
-			}
-			
-			if (eleccion1.equals("papel") && eleccion2.equals("papel")) {
-				System.out.println("Los dos eligieron papel, es un empate");
-				gameover = true;
-			}else if (eleccion1.equals("papel") && eleccion2.equals("piedra")) {
-				System.out.println("El jugador 1 gana. El papel envuelve a la piedra");
-				gameover = true;
-			}else if (eleccion1.equals("papel") && eleccion2.equals("tijera")) {
-				System.out.println("El jugador 2 gana. La tijera corta el papel");
-				gameover = true;
-			}
-			
-			if (eleccion1.equals("tijera") && eleccion2.equals("tijera")) {
-				System.out.println("Los dos eligieron tijera, es un empate");
-				gameover = true;
-			}else if (eleccion1.equals("tijera") && eleccion2.equals("papel")) {
-				System.out.println("El jugador 1 gana. La tijera corta el papel");
-				gameover = true;
-			}else if (eleccion1.equals("tijera") && eleccion2.equals("piedra")) {
-				System.out.println("El jugador 2 gana. La piedra quiebra la tijera");
-				gameover = true;
-			}
-			
-			entrada.close();
-		
-		
-	}
+		System.out.println("Empieza el juego!!!");
+		System.out.println("****************************************************");
+		System.out.printf("%s%n", "Jugador uno elija piedra papel o tijera: ");
+		eleccion1 = entrada.next();
 
+		System.out.printf("%s%n", "Jugador dos elija piedra papel o tijera: ");
+		eleccion2 = entrada.next();
+
+		entrada.close();
+
+		String decision = null;
+
+		if (eleccion1.equals("piedra") && eleccion2.equals("piedra")
+				|| eleccion1.equals("papel") && eleccion2.equals("papel")
+				|| eleccion1.equals("tijera") && eleccion2.equals("tijera")) {
+			decision = "empate";
+		} else if (eleccion1.equals("piedra") && eleccion2.equals("papel")
+				|| eleccion1.equals("papel") && eleccion2.equals("tijera")
+				|| eleccion1.equals("tijera") && eleccion2.equals("piedra")) {
+			decision = "gana2";
+		} else if (eleccion1.equals("piedra") && eleccion2.equals("tijera")
+				|| eleccion1.equals("papel") && eleccion2.equals("piedra")
+				|| eleccion1.equals("tijera") && eleccion2.equals("papel")) {
+			decision = "gana1";
+		}
+
+		switch (decision) {
+		case "empate":
+			System.out.println("Hicieron la misma elección, es un empate!!!");
+			break;
+		case "gana2":
+			System.out.println("El jugador 2 ganó!!!");
+			break;
+		case "gana1":
+			System.out.println("El jugador 1 ganó!!!");
+			break;
+		}
+	}
 }
